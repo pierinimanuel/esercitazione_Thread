913b695df449e34832385bb7fd91a23d16d8f390
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-05 09:51:29 +0100

    svolgimento

--- Esercitazione_thread/Program.cs
+++ Esercitazione_thread/Program.cs
@@ -11,17 +11,27 @@ namespace Esercitazione_thread
     {
         static void Main(string[] args)
         {
-            Thread myThread = new Thread(() =>
+            //Thread myThread = new Thread(() =>
+            //{
+            //    Console.WriteLine("MyThread è iniziato");
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("MyThread è terminato");
+            //});
+
+            ////esecuzione di myThread
+            //myThread.Start();
+            //Thread.Sleep(500);
+            //Console.WriteLine("Main Thread");
+
+            string someVariable = "Manuel Pierini";
+            var WorkerThread = new Thread((o) =>
             {
-                Console.WriteLine("MyThread è iniziato");
-                Thread.Sleep(1000);
-                Console.WriteLine("MyThread è terminato");
+                Console.WriteLine("Saluti da: {0}", 0);
             });
 
-            //esecuzione di myThread
-            myThread.Start();
-            Thread.Sleep(500);
-            Console.WriteLine("Main Thread");
+            WorkerThread.Start(someVariable);
         }
+
+       
     }
 }
