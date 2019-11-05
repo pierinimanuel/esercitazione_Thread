18b64ff8c416edf812bd3aafd6c5f6369b4bfc2d
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-05 09:40:13 +0100

    ho creato un istanza della classe thread alla quale ho assegnato un metodo definito tramite la labda expression, questo delegate viene preso in consegna myThread ed eseguito in un thread separato

---
+++ Esercitazione_thread/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Esercitazione_thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(() =>
+            {
+                Console.WriteLine("MyThread è iniziato");
+                Thread.Sleep(1000);
+                Console.WriteLine("MyThread è terminato");
+            });
+
+            //esecuzione di myThread
+            myThread.Start();
+            Thread.Sleep(500);
+            Console.WriteLine("Main Thread");
+        }
+    }
+}
